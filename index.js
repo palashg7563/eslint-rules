63a0e0c0374b6af3e0524aeb2a7c8dd46e030005
Palash Gupta <palashg7563@gmail.com>
2019-01-10 14:04:02 +0530

    added the script

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+/* eslint-disable no-console */
 const deperecated = require('./src/deprecated');
 const { recommendedRules } = require('./src/recommeded_rules');
+
+console.log(deperecated());
+
+recommendedRules()
+  .then(e => console.log(e))
+  .catch(e => console.log(e));
